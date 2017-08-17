56236b2a7ff33fa7125d44f6e2406cf4129f6555
Leonardo Brito <lbrito@gmail.com>
2017-08-17 18:29:54 -0300

    Adds custom attribute-matching support.

--- lib/matching_exceptions.rb
+++ lib/matching_exceptions.rb
@@ -3,16 +3,18 @@ require "matching_exceptions/version"
 module MatchingExceptions
   extend self
 
-  def matches(matching)
+  def matches(matching, attribute: 'message')
     klass = Class.new do
       def self.===(other)
-        return other.message.include?(@matching) if @matching.is_a?(String)
-        return other.message =~ @matching if @matching.is_a?(Regexp)
+        return false unless other.respond_to?(@attribute)
+        return other.send(@attribute).include?(@matching) if @matching.is_a?(String)
+        return other.send(@attribute) =~ @matching if @matching.is_a?(Regexp)
         false
       end
     end
 
     klass.instance_variable_set(:@matching, matching)
+    klass.instance_variable_set(:@attribute, attribute)
     klass
   end
 end
--- spec/matching_exceptions_spec.rb
+++ spec/matching_exceptions_spec.rb
@@ -1,6 +1,15 @@
 require "spec_helper"
 
 RSpec.describe MatchingExceptions do
+  class CustomError < StandardError
+    attr_reader :custom_attribute
+
+    def initialize(message, custom_attribute)
+      @custom_attribute = custom_attribute
+      super(message)
+    end
+  end
+
   it "has a version number" do
     expect(MatchingExceptions::VERSION).not_to be nil
   end
@@ -27,5 +36,30 @@ RSpec.describe MatchingExceptions do
     end
 
     expect(rescued).to be_truthy
+  end
+
+  it "rescues by specified attribute" do
+    rescued = false
+
+    begin
+      raise CustomError.new("blabla", "custom message")
+    rescue ME.matches(/message/, attribute: :custom_attribute)
+      rescued = true
+    end
+
+    expect(rescued).to be_truthy
+  end
+
+  it "ignores non existing attributes" do
+    rescued = false
+
+    begin
+      raise CustomError.new("blabla", "custom message")
+    rescue ME.matches(/message/, attribute: :foo)
+      rescued = true
+    rescue
+    end
+
+    expect(rescued).to be_falsy
   end
 end
