222509d7dc4cf3a0530afe101fa6ddce0cb047b9
tiagofsilva <aeroboom@gmail.com>
2017-08-19 02:45:19 -0300

    Solves problem where anonymous class inside module was being loaded on every #matches call

--- lib/matching_exceptions.rb
+++ lib/matching_exceptions.rb
@@ -1,21 +1,23 @@
 require "matching_exceptions/version"
 
 module MatchingExceptions
+  attr_accessor :klass
+
+  def self.extended(base)
+    define_method :=== do |other|
+      return false unless other.respond_to?(@method)
+      return other.send(@method).include?(@matching) if @matching.is_a?(String)
+      return other.send(@method) =~ @matching if @matching.is_a?(Regexp)
+      false
+    end
+  end
+
   extend self
 
   def matches(matching, on: 'message')
-    klass = Class.new do
-      def self.===(other)
-        return false unless other.respond_to?(@method)
-        return other.send(@method).include?(@matching) if @matching.is_a?(String)
-        return other.send(@method) =~ @matching if @matching.is_a?(Regexp)
-        false
-      end
-    end
-
-    klass.instance_variable_set(:@matching, matching)
-    klass.instance_variable_set(:@method, on)
-    klass
+    self.instance_variable_set(:@matching, matching)
+    self.instance_variable_set(:@method, on)
+    self
   end
 end
 
