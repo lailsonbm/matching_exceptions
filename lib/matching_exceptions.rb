29b81da82856ba24287166fa2d64e41b3805d53f
tiagofsilva <aeroboom@gmail.com>
2017-08-22 03:14:48 -0300

    Changes private method names to follow ruby convention for boolean methods

--- lib/matching_exceptions.rb
+++ lib/matching_exceptions.rb
@@ -5,18 +5,18 @@ module MatchingExceptions
   def self.extended(base)
     define_method :=== do |error_message|
       error_message.respond_to?(@method) && 
-        (matches_string(error_message) || matches_regex(error_message))
+        (matches_string?(error_message) || matches_regex?(error_message))
     end
 
-    define_method :matches_string do |error_message|
+    define_method :matches_string? do |error_message|
       @matching.is_a?(String) && error_message.send(@method).include?(@matching)
     end
 
-    define_method :matches_regex do |error_message|
+    define_method :matches_regex? do |error_message|
       @matching.is_a?(Regexp) && error_message.send(@method) =~ @matching
     end
 
-    private :===, :matches_string, :matches_regex
+    private :===, :matches_string?, :matches_regex?
   end
 
   extend self
